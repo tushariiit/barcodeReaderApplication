13a4c1be3c7ae9cad6ddfefb77cb45d17a1ee121
tushariiit <tushariiit@users.noreply.github.com>
2020-12-01 18:03:23 +0530

    3rd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:barcode_scan/barcode_scan.dart';
-
 
 void main() => runApp(MyApp());
 
@@ -13,6 +13,42 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
+    );
+  }
+}
+class Draweronly extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return new Drawer(
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+            DrawerHeader(
+              child: Text('Hi User!!', style: TextStyle(fontSize: 40.0), textAlign: TextAlign.center,),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+          ListTile(
+            leading: Icon(Icons.settings), title: Text("How to Use"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(context, new MaterialPageRoute(builder: (ctxt) => new howToUse()));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.contacts), title: Text("Contact Us"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(context, new MaterialPageRoute(builder: (ctxt) => new contactus()));
+            },
+          ),
+        ],
+      ),
     );
   }
 }
@@ -83,44 +119,25 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-        drawer: Drawer(
-          // Add a ListView to the drawer. This ensures the user can scroll
-          // through the options in the drawer if there isn't enough vertical
-          // space to fit everything.
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Text('Hi User!!', style: TextStyle(fontSize: 40.0), textAlign: TextAlign.center,),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.home), title: Text("Home"),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.settings), title: Text("Settings"),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.contacts), title: Text("Contact Us"),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-            ],
-          ),
-        ),
+        drawer: new Draweronly(),
+    );
+  }
+}
+class contactus extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Contact Us"),),
+      body: new Text("Here will be contact Details"),
+    );
+  }
+}
+class howToUse extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("How to Use"),),
+      body: new Text("Here will be Instructions to use"),
     );
   }
 }
